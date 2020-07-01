7162fa3ffa031109a05612d3c34ce707d22c1074
Nikolay Yuush <bawerek@gmail.com>
2020-07-01 21:10:11 +0300

    final version of the accordion

--- src/js/module/accordion.js
+++ src/js/module/accordion.js
@@ -1,25 +1,16 @@
 export class Accordion {
 		constructor({btn, block}) {
 				this.btns = document.querySelectorAll(btn);
-				this.block = document.querySelectorAll(block);
 		}
 		init() {
 				// this.setAnimation()
 				this.btns.forEach((btn, i) => {
 						btn.addEventListener('click', (e)=>{
-								if (this.block[i].style.display === 'block') {
-										this.block[i].style.display = 'none'
-										this.block[i].classList.remove('animation', 'fadeIn')
-								} else {
-										this.block[i].classList.add('animation', 'fadeIn')
-										this.block[i].style.display = 'block'
-								}
+								const sibling = btn.closest('.module__info-show').nextElementSibling;
+								sibling.style.marginTop = '20px';
+								sibling.classList.add('animated', 'flipInY');
+								sibling.classList.toggle('msg');
 						})
-				})
-		}
-		setAnimation() {
-				this.block.forEach(block => {
-						block.classList.add('animation', 'fadeIn')
 				})
 		}
 }
